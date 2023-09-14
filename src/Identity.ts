938a065da71898b3c836d1c3593d955f4a2e9404
John Williams <williamsjohn@microsoft.com>
2023-09-14 15:51:18 -0700

    Alphabetized symbols of Identity and its tests

--- src/Identity.ts
+++ src/Identity.ts
@@ -1,4 +1,3 @@
-import { ImmediatelyInvokedFunctionExpression } from "../node_modules/typescript/lib/typescript";
 import { Ok, Result } from "./Result";
 import { Tappable } from "./Tappable";
 
@@ -20,16 +19,6 @@ export function Identity<T>(value: T): Identity<T> {
 }
 
 /**
- * Tests whether `arg` is an `Identity` object.
- */
-export function isIdentity(arg: unknown): arg is Identity<unknown> {
-  return arg instanceof IdentityImpl;
-}
-
-// @copy-comment
-Identity.isIdentity = isIdentity;
-
-/**
  * Tests whether `a` and `b` are `Identity` objects that are equal according to
  * `a.equals(b, cmp)`.
  *
@@ -43,6 +32,16 @@ Identity.equals = (
   return isIdentity(a) && isIdentity(b) && a.equals(b, cmp);
 };
 
+/**
+ * Tests whether `arg` is an `Identity` object.
+ */
+export function isIdentity(arg: unknown): arg is Identity<unknown> {
+  return arg instanceof IdentityImpl;
+}
+
+// @copy-comment
+Identity.isIdentity = isIdentity;
+
 export class IdentityImpl<T> extends Tappable implements Iterable<T> {
   constructor(
     /**
@@ -51,6 +50,66 @@ export class IdentityImpl<T> extends Tappable implements Iterable<T> {
     readonly value: T,
   ) {
     super();
+  }
+
+  /**
+   * Returns an iterator that yields `this.value`.
+   */
+  [Symbol.iterator](): Iterator<T> {
+    return [this.value].values();
+  }
+
+  /**
+   * Returns `f(this.value)`.
+   */
+  andThen<R>(f: (value: T) => Identity<R>): Identity<R> {
+    return f(this.value);
+  }
+
+  /**
+   * Tests if two `Identity` values are equal, i.e. `Identity(x)`
+   * and `Identity(y)`, where `x` and `y` are equal.
+   *
+   * By default, `x` and `y` are compared with `equal` if they both contain
+   * `Identity` values; otherwise they are compared using `===`.
+   *
+   * If `cmp` is supplied, it is used in place of the default equality
+   * logic.
+   */
+  equals<U>(that: Identity<U>, cmp?: (a: T, b: U) => boolean): boolean {
+    return cmp
+      ? cmp(this.value, that.value)
+      : isIdentity(this.value) && isIdentity(that.value)
+      ? this.value.equals(that.value)
+      : (this.value as unknown) === (that.value as unknown);
+  }
+
+  /**
+   * Returns `this.value`.
+   */
+  expect(_: unknown): T {
+    return this.value;
+  }
+
+  /**
+   * Alias of {@link andThen}.
+   */
+  flatMap<R>(f: (value: T) => Identity<R>): Identity<R> {
+    return f(this.value);
+  }
+
+  /**
+   * Returns `this.value`.
+   */
+  flatten<T>(this: Identity<Identity<T>>): Identity<T> {
+    return this.value;
+  }
+
+  /**
+   * Alias of {@link flatten}.
+   */
+  join<T>(this: Identity<Identity<T>>): Identity<T> {
+    return this.value;
   }
 
   /**
@@ -65,6 +124,53 @@ export class IdentityImpl<T> extends Tappable implements Iterable<T> {
    */
   isIdentityAnd(p: (arg: T) => boolean): boolean {
     return p(this.value);
+  }
+
+  /**
+   * Returns `Identity(f(this.value))`.
+   */
+  map<R>(f: (value: T) => R): Identity<R> {
+    return Identity(f(this.value));
+  }
+
+  /**
+   * Returns `f(this.value)`.
+   */
+  mapOr<R>(_: unknown, f: (value: T) => R): R {
+    return f(this.value);
+  }
+
+  /**
+   * Returns `f(this.value)`.
+   */
+  mapOrElse<R>(_: unknown, f: (value: T) => R): R {
+    return f(this.value);
+  }
+
+  /**
+   * Returns `Ok(this.value)`.
+   */
+  okOr<E>(_: unknown): Result<T, E> {
+    return Ok(this.value);
+  }
+
+  /**
+   * Returns `Ok(this.value)`.
+   */
+  okOrElse<E>(_: () => E): Result<T, E> {
+    return Ok(this.value);
+  }
+
+  /**
+   * Calls `f(this.value)` and returns `this`.
+   */
+  tapIdentity(f: (value: T) => void): this {
+    f(this.value);
+    return this;
+  }
+
+  toString(): string {
+    return `Identity(${this.value})`;
   }
 
   /**
@@ -98,121 +204,20 @@ export class IdentityImpl<T> extends Tappable implements Iterable<T> {
   /**
    * Returns `this.value`.
    */
-  expect(_: unknown): T {
-    return this.value;
-  }
-
-  /**
-   * Returns `this.value`.
-   */
   unwrapUnchecked(arg?: unknown): T {
     return this.value;
   }
 
   /**
-   * Returns `Ok(this.value)`.
+   * Returns `[this.value, other.value]`.
    */
-  okOr<E>(_: unknown): Result<T, E> {
-    return Ok(this.value);
-  }
-
-  /**
-   * Returns `Ok(this.value)`.
-   */
-  okOrElse<E>(_: () => E): Result<T, E> {
-    return Ok(this.value);
-  }
-
-  /**
-   * Calls `f(this.value)` and returns `this`.
-   */
-  tapIdentity(f: (value: T) => void): this {
-    f(this.value);
-    return this;
-  }
-
-  /**
-   * Returns `Identity(f(this.value))`.
-   */
-  map<R>(f: (value: T) => R): Identity<R> {
-    return Identity(f(this.value));
-  }
-
-  /**
-   * Returns `f(this.value)`.
-   */
-  mapOr<R>(_: unknown, f: (value: T) => R): R {
-    return f(this.value);
-  }
-
-  /**
-   * Returns `f(this.value)`.
-   */
-  mapOrElse<R>(_: unknown, f: (value: T) => R): R {
-    return f(this.value);
-  }
-
-  /**
-   * Returns `f(this.value)`.
-   */
-  andThen<R>(f: (value: T) => Identity<R>): Identity<R> {
-    return f(this.value);
-  }
-
-  /**
-   * Alias of {@link andThen}.
-   */
-  flatMap<R>(f: (value: T) => Identity<R>): Identity<R> {
-    return f(this.value);
-  }
-
-  /**
-   * Returns `this.value`.
-   */
-  flatten<T>(this: Identity<Identity<T>>): Identity<T> {
-    return this.value;
-  }
-
-  /**
-   * Alias of {@link flatten}.
-   */
-  join<T>(this: Identity<Identity<T>>): Identity<T> {
-    return this.value;
-  }
-
-  /**
-   * Tests if two `Identity` values are equal, i.e. `Identity(x)`
-   * and `Identity(y)`, where `x` and `y` are equal.
-   *
-   * By default, `x` and `y` are compared with `equal` if they both contain
-   * `Identity` values; otherwise they are compared using `===`.
-   *
-   * If `cmp` is supplied, it is used in place of the default equality
-   * logic.
-   */
-  equals<U>(that: Identity<U>, cmp?: (a: T, b: U) => boolean): boolean {
-    return cmp
-      ? cmp(this.value, that.value)
-      : isIdentity(this.value) && isIdentity(that.value)
-        ? this.value.equals(that.value)
-        : (this.value as unknown) === (that.value as unknown);
-  }
-
-  /**
-   * Returns an iterator that yields `this.value`.
-   */
-  [Symbol.iterator](): Iterator<T> {
-    return [this.value].values();
-  }
-
-  toString(): string {
-    return `Identity(${this.value})`;
-  }
-
   zip<U>(other: Identity<U>): Identity<[T, U]> {
     return Identity([this.value, other.value] as [T, U]);
   }
 
+  /**
+   * Returns `f(this.value, other.value)`.
+   */
   zipWith<U, R>(other: Identity<U>, f: (a: T, b: U) => R): Identity<R> {
     return Identity(f(this.value, other.value));
   }
--- test/Identity.test.ts
+++ test/Identity.test.ts
@@ -25,24 +25,49 @@ describe("Identity functions", () => {
     expect(Identity.isIdentity).toBe(isIdentity);
   });
 
-  test("isIdentity", () => {
-    expect(isIdentity(Identity(0))).toBe(true);
-    expect(isIdentity(null)).toBe(false);
-    expect(isIdentity(undefined)).toBe(false);
-    expect(isIdentity(theT)).toBe(false);
-  });
-
   test("Identity.equals", () => {
     expect(Identity.equals(theT, theT)).toBe(false);
     expect(Identity.equals(Identity(theT), theT)).toBe(false);
     expect(Identity.equals(theT, Identity(theT))).toBe(false);
     testEqualsFn(Identity.equals);
   });
+
+  test("isIdentity", () => {
+    expect(isIdentity(Identity(0))).toBe(true);
+    expect(isIdentity(null)).toBe(false);
+    expect(isIdentity(undefined)).toBe(false);
+    expect(isIdentity(theT)).toBe(false);
+  });
 });
 
 describe("Identity methods", () => {
-  test("value", () => {
-    expect(Identity(theT).value).toBe(theT);
+  test("@@iterator", () => {
+    expect(Array.from(Identity(theT))).toEqual([theT]);
+  });
+
+  test.each([
+    ["andThen", (x: Identity<T>, f: (arg: T) => Identity<R>) => x.andThen(f)],
+    ["flatMap", (x: Identity<T>, f: (arg: T) => Identity<R>) => x.flatMap(f)],
+  ])("%s", (_name, andThen) => {
+    expect(
+      andThen(Identity(theT), expectArgs(Identity(theR), theT)).unwrap(),
+    ).toBe(theR);
+    expect(andThen(Identity(theT), expectArgs(Identity(theR), theT))).toEqual(
+      Identity(theR),
+    );
+  });
+
+  test("equals", () => {
+    testEqualsFn((a, b, cmp) =>
+      (a as Identity<unknown>).equals(b as Identity<unknown>, cmp),
+    );
+  });
+
+  test.each([
+    ["flatten", (x: Identity<Identity<T>>) => x.flatten()],
+    ["join", (x: Identity<Identity<T>>) => x.join()],
+  ])("%s", (_name, flatten) => {
+    expect(flatten(Identity(Identity(theT))).unwrap()).toBe(theT);
   });
 
   test("isIdentityAnd", () => {
@@ -61,38 +86,6 @@ describe("Identity methods", () => {
     expect(Identity(theT).expect(notCalled)).toBe(theT);
   });
 
-  test("unwrap", () => {
-    expect(Identity(theT).unwrap()).toBe(theT);
-    expect(Identity(theT).unwrap(notCalled)).toBe(theT);
-  });
-
-  test("unwrapOr", () => {
-    expect(Identity(theT).unwrapOr(theE)).toBe(theT);
-  });
-
-  test("unwrapOrElse", () => {
-    expect(Identity(theT).unwrapOrElse(notCalled)).toBe(theT);
-  });
-
-  test("unwrapUnchecked", () => {
-    expect(Identity(theT).unwrapUnchecked()).toBe(theT);
-  });
-
-  test.each([["unwrapOrUndef", (x: Identity<T>) => x.unwrapOrUndef()]])(
-    "%s",
-    (_name, unwrapOrUndef) => {
-      expect(Identity(theT).unwrapOrUndef()).toBe(theT);
-    },
-  );
-
-  test("okOr", () => {
-    expect(Identity(theT).okOr(theE).unwrap()).toBe(theT);
-  });
-
-  test("okOrElse", () => {
-    expect(Identity(theT).okOrElse(notCalled).unwrap()).toBe(theT);
-  });
-
   test("map", () => {
     expect(Identity(theT).map(expectArgs(theR, theT)).unwrap()).toBe(theR);
   });
@@ -105,6 +98,14 @@ describe("Identity methods", () => {
     expect(Identity(theT).mapOrElse(notCalled, expectArgs(theR, theT))).toBe(
       theR,
     );
+  });
+
+  test("okOr", () => {
+    expect(Identity(theT).okOr(theE).unwrap()).toBe(theT);
+  });
+
+  test("okOrElse", () => {
+    expect(Identity(theT).okOrElse(notCalled).unwrap()).toBe(theT);
   });
 
   test("tap", () => {
@@ -121,24 +122,40 @@ describe("Identity methods", () => {
     expect(mockFunc).toHaveBeenCalledTimes(1);
   });
 
-  test.each([
-    ["andThen", (x: Identity<T>, f: (arg: T) => Identity<R>) => x.andThen(f)],
-    ["flatMap", (x: Identity<T>, f: (arg: T) => Identity<R>) => x.flatMap(f)],
-  ])("%s", (_name, andThen) => {
-    expect(
-      andThen(Identity(theT), expectArgs(Identity(theR), theT)).unwrap(),
-    ).toBe(theR);
-    expect(andThen(Identity(theT), expectArgs(Identity(theR), theT))).toEqual(
-      Identity(theR),
-    );
+  test("toString", () => {
+    expect(Identity(42).toString()).toBe("Identity(42)");
+  });
+
+  test("unwrap", () => {
+    expect(Identity(theT).unwrap()).toBe(theT);
+    expect(Identity(theT).unwrap(notCalled)).toBe(theT);
+  });
+
+  test("unwrapOr", () => {
+    expect(Identity(theT).unwrapOr(theE)).toBe(theT);
+  });
+
+  test("unwrapOrElse", () => {
+    expect(Identity(theT).unwrapOrElse(notCalled)).toBe(theT);
+  });
+
+  test.each([["unwrapOrUndef", (x: Identity<T>) => x.unwrapOrUndef()]])(
+    "%s",
+    (_name, unwrapOrUndef) => {
+      expect(Identity(theT).unwrapOrUndef()).toBe(theT);
+    },
+  );
+
+  test("unwrapUnchecked", () => {
+    expect(Identity(theT).unwrapUnchecked()).toBe(theT);
+  });
+
+  test("value", () => {
+    expect(Identity(theT).value).toBe(theT);
   });
 
   test("zipWith", () => {
-    expect(
-      Identity(theT)
-        .zip(Identity(theE))
-        .unwrap(),
-    ).toEqual([theT, theE]);
+    expect(Identity(theT).zip(Identity(theE)).unwrap()).toEqual([theT, theE]);
   });
 
   test("zipWith", () => {
@@ -147,27 +164,6 @@ describe("Identity methods", () => {
         .zipWith(Identity(theE), expectArgs(theR, theT, theE))
         .unwrap(),
     ).toBe(theR);
-  });
-
-  test.each([
-    ["flatten", (x: Identity<Identity<T>>) => x.flatten()],
-    ["join", (x: Identity<Identity<T>>) => x.join()],
-  ])("%s", (_name, flatten) => {
-    expect(flatten(Identity(Identity(theT))).unwrap()).toBe(theT);
-  });
-
-  test("equals", () => {
-    testEqualsFn((a, b, cmp) =>
-      (a as Identity<unknown>).equals(b as Identity<unknown>, cmp),
-    );
-  });
-
-  test("@@iterator", () => {
-    expect(Array.from(Identity(theT))).toEqual([theT]);
-  });
-
-  test("toString", () => {
-    expect(Identity(42).toString()).toBe("Identity(42)");
   });
 });
 
